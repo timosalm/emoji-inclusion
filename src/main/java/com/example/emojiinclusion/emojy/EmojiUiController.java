3c3055c71d225fee122d9c696f59d24d0b813ee2
Timo Salm <tsalm@vmware.com>
2023-06-01 15:51:01 +0200

    Add information for used database to UI

--- src/main/java/com/example/emojiinclusion/emojy/EmojiUiController.java
+++ src/main/java/com/example/emojiinclusion/emojy/EmojiUiController.java
@@ -3,6 +3,7 @@ package com.example.emojiinclusion.emojy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.stream.Collectors;
 
@@ -10,6 +11,9 @@ import java.util.stream.Collectors;
 public class EmojiUiController {
 
     private final EmojiApplicationService applicationService;
+
+    @Value("${spring.datasource.url}")
+    private String dataSourceUrl;
 
     public EmojiUiController(final EmojiApplicationService applicationService) {
         this.applicationService = applicationService;
@@ -19,6 +23,9 @@ public class EmojiUiController {
     public String fetchUI(Model model) {
         model.addAttribute("emojis", applicationService.fetchEmojis().stream().limit(6).map(Emoji::getStringValue)
                 .collect(Collectors.joining ("")));
+
+        var dataSourceUrlSubstring = dataSourceUrl.replace("jdbc:", "");
+        model.addAttribute("database", dataSourceUrlSubstring.substring(0, dataSourceUrlSubstring.indexOf(":")).toUpperCase());
         return "index";
     }
 }
